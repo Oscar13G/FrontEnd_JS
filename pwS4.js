a6fbf2c0607076ee26adf0dcc8fc79522e141706
Oscar13G <oscar.glz.ing@gmail.com>
2022-07-30 16:36:05 -0500

    Creacion de Ramas

--- pwS4.js
+++ pwS4.js
@@ -140,7 +140,7 @@ function frequency(string) {
     for (let i = 0; i < string.length; i++)
         cadena2[i] = string[i];
     //  Ordena el array alfabeticamente
-    console.log(cadena2.sort());
+    cadena2.sort();
     //  Ciclo mientras la cadena contenga caracteres
     while (cadena2.length > 0) {
         let stringRigth = numRepeticion(cadena2, cadena2[0]);
@@ -152,6 +152,7 @@ function frequency(string) {
     return ans;
 }
 // Pruebas
+console.log('Test 1:', frequency('Vicente Barbosa Leon'));
 console.log('Test 1:', frequency('cccbbbaaa'))// {a: 3, b: 3, c: 3}
 console.log('Test 2:', frequency('www.bedu.org'))// {.: 2, b: 1, d: 1, e: 1, g: 1, o: 1, r: 1, u: 1, w: 3}
 console.log('Test 3:', frequency('john.doe@domain.com'))// {.: 2, @: 1, a: 1, c: 1, d: 2, e: 1, h: 1, i: 1, j: 1, m: 2, n: 2, o: 4}
