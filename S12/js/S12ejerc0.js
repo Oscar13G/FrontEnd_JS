46e01042e5ae9c98ed4f058b42bbec5ca56033e7
Oscar13G <oscar.glz.ing@gmail.com>
2022-09-05 17:29:50 -0500

    S12 --> Pendiente Retos

---
+++ S12/js/S12ejerc0.js
@@ -0,0 +1,15 @@
+const data = {
+  username: "Listo",
+  email: "jonh@doe.com",
+};
+
+fetch("http:example.com/data.json", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(data),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
